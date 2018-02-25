3566d1d3bac82b1cea5484c0af53021f59254a8f
leonbec <dwfinz@gmail.com>
2018-02-26 09:14:33 +1300

    init

---
+++ src/templates/page-template.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import PageTemplateDetails from '../components/PageTemplateDetails';
+
+class PageTemplate extends React.Component {
+  render() {
+    const { title, subtitle } = this.props.data.site.siteMetadata;
+    const page = this.props.data.markdownRemark;
+    const { title: pageTitle, description: pageDescription } = page.frontmatter;
+    const description = pageDescription !== null ? pageDescription : subtitle;
+
+    return (
+      <div>
+        <Helmet>
+          <title>{`${pageTitle} - ${title}`}</title>
+          <meta name="description" content={description} />
+        </Helmet>
+        <PageTemplateDetails {...this.props} />
+      </div>
+    );
+  }
+}
+
+export default PageTemplate;
+
+export const pageQuery = graphql`
+  query PageBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        menu {
+          label
+          path
+        }
+        author {
+          name
+          email
+          telegram
+          twitter
+          github
+          rss
+          vk
+        }
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+        date
+        description
+      }
+    }
+  }
+`;
---
+++ src/templates/tag-template.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Sidebar from '../components/Sidebar';
+import TagTemplateDetails from '../components/TagTemplateDetails';
+
+class TagTemplate extends React.Component {
+  render() {
+    const { title } = this.props.data.site.siteMetadata;
+    const { tag } = this.props.pathContext;
+
+    return (
+      <div>
+        <Helmet title={`All Posts tagged as "${tag}" - ${title}`} />
+        <Sidebar {...this.props} />
+        <TagTemplateDetails {...this.props} />
+      </div>
+    );
+  }
+}
+
+export default TagTemplate;
+
+export const pageQuery = graphql`
+  query TagPage($tag: String) {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        menu {
+          label
+          path
+        }
+        author {
+          name
+          email
+          telegram
+          twitter
+          github
+          rss
+          vk
+        }
+      }
+    }
+    allMarkdownRemark(
+        limit: 1000,
+        filter: { frontmatter: { tags: { in: [$tag] }, layout: { eq: "post" }, draft: { ne: true } } },
+        sort: { order: DESC, fields: [frontmatter___date] }
+      ){
+      edges {
+        node {
+          fields {
+            slug
+            categorySlug
+          }
+          frontmatter {
+            title
+            date
+            category
+            description
+          }
+        }
+      }
+    }
+  }
+`;
